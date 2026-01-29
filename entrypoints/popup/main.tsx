bc25449d6a89b157342902512c476748f6052126
Sasha Marchuk <Sasha.Marchuk.email@gmail.com>
2026-01-29 10:37:50 +0700

    feat(06-02): await store hydration before rendering popup
    - Import storeReadyPromise from store
    - Show loading state while store hydrates from chrome.storage
    - Render React app inside .then() callback
    - Prevents UI flash of default values

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import '../../src/assets/app.css';
+import { storeReadyPromise } from '../../src/store';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Show loading state while store hydrates
+const rootElement = document.getElementById('root')!;
+rootElement.innerHTML = '<div style="padding: 16px; color: #666;">Loading...</div>';
+
+// Wait for store to hydrate from chrome.storage before rendering
+storeReadyPromise.then(() => {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
